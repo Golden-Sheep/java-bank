daa3ab69769870089cf09bd7c212031001642e1a
JOAO VICTOR SILVA <dev.jvictor@gmail.com>
2025-07-04 22:36:15 -0300

    Closes #5 - Fix contextLoads Failure in Integration Tests

--- src/test/java/dev/jvops/bank/JavaBankApplicationTests.java
+++ src/test/java/dev/jvops/bank/JavaBankApplicationTests.java
@@ -2,8 +2,10 @@ package dev.jvops.bank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JavaBankApplicationTests {
 
     @Test
